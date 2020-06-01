00d3d4294efed0b68e38a41bc69582f7ee7f982a
MhrjAniiz <anishmaharjan17@gmail.com>
2020-06-01 16:55:46 +0545

    added email field with first and lastname as well as password retrieval is also implemented using smtp and gmail

--- rojgar/settings.py
+++ rojgar/settings.py
@@ -119,3 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#SMTP configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST =  'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS =True
+EMAIL_HOST_USER = 'anishmaharjan1998921@gmail.com'
+EMAIL_HOST_PASSWORD = 'Django_password'
--- signin/views.py
+++ signin/views.py
@@ -3,6 +3,7 @@ from django.db import IntegrityError
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
 from django.contrib.auth.models import User
+from .forms import RegistrationForm
 
 
 def home(request):
@@ -11,20 +12,23 @@ def home(request):
 
 def signup(request):
     if request.method == 'GET':
-        return render(request, 'signup.html', {'form': UserCreationForm})
+        return render(request, 'signup.html', {'form': RegistrationForm})
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
-                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user = User.objects.create_user(request.POST['username'],
+                        password=request.POST['password1'],
+                        email=request.POST['email'],
+                        first_name=request.POST['first_name'],last_name= request.POST['last_name'])
                 user.save()
                 login(request, user)
                 return redirect('results')
             except IntegrityError:
                 return render(request, 'signup.html',
-                {'form': UserCreationForm, 'error': 'the username has already been used'})
+                {'form': RegistrationForm, 'error': 'the username has already been used'})
         else:
             return render(request, 'signup.html',
-                          {'form': UserCreationForm, 'error': 'the password did not match'})
+                          {'form': RegistrationForm, 'error': 'the password did not match'})
 
 
 def loginuser(request):
@@ -47,3 +51,5 @@ def logoutuser(request):
 
 def results(request):
     return render(request,'results.html')
+
+

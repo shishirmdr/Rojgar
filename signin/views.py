2bf48bfc8be198c825004561870e6ad480c74b40
Agrim <agrim.tuladhar@gmail.com>
2020-06-02 20:32:22 +0545

    removed that svg code

--- signin/views.py
+++ signin/views.py
@@ -27,10 +27,10 @@ def signup(request):
                 return redirect('results')
             except IntegrityError:
                 return render(request, template,
-                              {'form': RegistrationForm, 'error': 'the username has already been used'})
+                              {'form': RegistrationForm, 'error': 'The username has already been used.'})
         else:
             return render(request, template,
-                          {'form': RegistrationForm, 'error': 'the password did not match'})
+                          {'form': RegistrationForm, 'error': 'The passwords did not match.'})
 
 
 
@@ -42,7 +42,7 @@ def loginuser(request):
     else:
         user = authenticate(request,username=request.POST['username'], password=request.POST['password'])
         if user is None:
-            return render(request, template,{'form':AuthenticationForm,'error':"the username and the password didn't match"})
+            return render(request, template,{'form':AuthenticationForm,'error':"The username and the password did not match."})
         else:
             login(request, user)
             return redirect("results")
